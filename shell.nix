d9c06791cc3b728485d29c9c9e472c3b84ce5c9a
John Axel Eriksson <john@insane.se>
2019-03-21 23:45:16 +0100

    Updates

--- shell.nix
+++ shell.nix
@@ -1,6 +1,18 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "moonbar";
+  buildInputs = [
+    curl
+    jq
+    iw
+    gnugrep
+    gawk
+    gsimplecal
+    bind
+    acpi
+    notify-desktop
+    pulseaudio
+  ];
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.systemd.lib}/lib
   '';
